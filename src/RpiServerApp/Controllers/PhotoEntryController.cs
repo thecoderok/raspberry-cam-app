9f1298c03f87d828a68b5bc67e01e86b7b020755
the-coderok <vitaliiy@outlook.com>
2017-03-15 09:56:42 -0700

    dummy commit

--- src/RpiServerApp/Controllers/PhotoEntryController.cs
+++ src/RpiServerApp/Controllers/PhotoEntryController.cs
@@ -2,7 +2,6 @@ namespace RpiProject.Controllers
 {
     using System;
     using System.Collections.Generic;
-    using System.Globalization;
     using System.IO;
     using System.Linq;
     using System.Threading.Tasks;
